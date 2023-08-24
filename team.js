68cbf2e209a6db9ce248a4d2c7e53b611cc83101
St3fan097 <Ste97.esse@gmail.com>
2023-08-24 09:05:15 +0200

    creazione file prima parte

---
+++ team.js
@@ -0,0 +1,17 @@
+let gruppo = [
+    {
+        name: "Stefano",
+        surname: "Sala",
+        age: 26,
+        city: "Milano",
+        hobby: "Gaming",
+        favoriteFood: "Pizza",
+        favoriteVideoGame: "COD",
+        favoriteFilm: "Interstellar",
+        favoriteBook: "",
+        petName: "Totò"
+    }
+    //prossima persona
+]
+
+//funzioni per seconda parte
