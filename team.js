5cbc3c75a5eb6a4a9ec811f0300c1cd724907e17
Marta <marta.ferraris25@gmail.com>
2023-08-25 19:38:10 +0200

    Ho creato il mio branch e fatto il mio pezzo

--- team.js
+++ team.js
@@ -10,8 +10,20 @@ let gruppo = [
         favoriteFilm: "Interstellar",
         favoriteBook: "",
         petName: "Totò"
-    }
-    //prossima persona
+    },
+    {
+        name: "Marta",
+        surname: "Ferraris",
+        age: 29,
+        city: "Soprana",
+        hobby: "Illustration",
+        favoriteFood: "Chirashi sake",
+        favoriteVideoGame: "AC Odyssey",
+        favoriteFilm: "Spirit away",
+        favoriteBook: "a Touch of Darkness",
+        petName: "Cleo & Mia",
+    },
+    // prossima persona
 ]
 
 //funzioni per seconda parte
