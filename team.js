de95afd010e951e4e8cd7705b2dfaab3b0d24642
Adolfo-97 <adolfogomez@live.it>
2023-08-26 09:08:26 +0200

    terzo commit

--- team.js
+++ team.js
@@ -1,29 +1,40 @@
 let gruppo = [
-    {
-        name: "Stefano",
-        surname: "Sala",
-        age: 26,
-        city: "Milano",
-        hobby: "Gaming",
-        favoriteFood: "Pizza",
-        favoriteVideoGame: "COD",
-        favoriteFilm: "Interstellar",
-        favoriteBook: "",
-        petName: "Totò"
-    },
-    {
-        name: "Marta",
-        surname: "Ferraris",
-        age: 29,
-        city: "Soprana",
-        hobby: "Illustration",
-        favoriteFood: "Chirashi sake",
-        favoriteVideoGame: "AC Odyssey",
-        favoriteFilm: "Spirit away",
-        favoriteBook: "a Touch of Darkness",
-        petName: "Cleo & Mia",
-    },
-    // prossima persona
-]
+  {
+    name: "Stefano",
+    surname: "Sala",
+    age: 26,
+    city: "Milano",
+    hobby: "Gaming",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "COD",
+    favoriteFilm: "Interstellar",
+    favoriteBook: "",
+    petName: "Totò",
+  },
+  {
+    name: "Marta",
+    surname: "Ferraris",
+    age: 29,
+    city: "Soprana",
+    hobby: "Illustration",
+    favoriteFood: "Chirashi sake",
+    favoriteVideoGame: "AC Odyssey",
+    favoriteFilm: "Spirit away",
+    favoriteBook: "a Touch of Darkness",
+    petName: "Cleo & Mia",
+  },
+  {
+    name: "Adolfo",
+    surname: "Gomez",
+    age: 25,
+    city: "Viterbo",
+    hobby: "Photography",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "Iracing",
+    favoriteFilm: "Pulp Fiction",
+    favoriteBook: "",
+    petName: "Peter",
+  },
+];
 
 //funzioni per seconda parte
